edc59e41d15a4f47fe89e5831be759b41aae727f
IsraelRAlencar <israelalencar@gecontrolsistemas.com.br>
2024-03-17 08:44:35 -0400

    new - refatorando ProductGrid

--- lib/pages/products_overview_pages.dart
+++ lib/pages/products_overview_pages.dart
@@ -9,24 +9,33 @@ class ProductsOverviewPage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    final provider = Provider.of<ProductList>(context);
-    final List<Product> loadedProducts = provider.items;
-
     return Scaffold(
       appBar: AppBar(
         title: const Text('Minha Loja'),
         //backgroundColor: Theme.of(context).colorScheme.primary,
       ),
-      body: GridView.builder(
-          padding: const EdgeInsets.all(10),
-          itemCount: loadedProducts.length,
-          gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
-            crossAxisCount: 2,
-            childAspectRatio: 3 / 2,
-            crossAxisSpacing: 10,
-            mainAxisSpacing: 10,
-          ),
-          itemBuilder: (ctx, i) => ProductItem(product: loadedProducts[i])),
+      body: const ProductGrid(),
     );
+  }
+}
+
+class ProductGrid extends StatelessWidget {
+  const ProductGrid({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    final provider = Provider.of<ProductList>(context);
+    final List<Product> loadedProducts = provider.items;
+
+    return GridView.builder(
+        padding: const EdgeInsets.all(10),
+        itemCount: loadedProducts.length,
+        gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+          crossAxisCount: 2,
+          childAspectRatio: 3 / 2,
+          crossAxisSpacing: 10,
+          mainAxisSpacing: 10,
+        ),
+        itemBuilder: (ctx, i) => ProductItem(product: loadedProducts[i]));
   }
 }
