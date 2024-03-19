4c9ab36863dbe2c75f07f2c52a8291283791a98b
IsraelRAlencar <israelalencar@gecontrolsistemas.com.br>
2024-03-18 21:06:48 -0400

    new - criando campo url imagem com update automatico ao sair do campo

--- lib/pages/product_form_page.dart
+++ lib/pages/product_form_page.dart
@@ -10,12 +10,27 @@ class ProductFormPage extends StatefulWidget {
 class _ProductFormPageState extends State<ProductFormPage> {
   final _priceFocus = FocusNode();
   final _descriptionFocus = FocusNode();
+  final _imageUrlFocus = FocusNode();
+  final _imageUrlController = TextEditingController();
 
   @override
   void dispose() {
     super.dispose();
     _priceFocus.dispose();
     _descriptionFocus.dispose();
+    _imageUrlFocus.dispose();
+    _imageUrlFocus.removeListener(_updateImageUrl);
+    _imageUrlController.dispose();
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    _imageUrlFocus.addListener(_updateImageUrl);
+  }
+
+  _updateImageUrl() {
+    setState(() {});
   }
 
   @override
@@ -53,6 +68,45 @@ class _ProductFormPageState extends State<ProductFormPage> {
                 focusNode: _descriptionFocus,
                 keyboardType: TextInputType.multiline,
                 maxLines: 3,
+                onFieldSubmitted: (_) {
+                  FocusScope.of(context).requestFocus(_imageUrlFocus);
+                },
+              ),
+              Row(
+                crossAxisAlignment: CrossAxisAlignment.end,
+                children: [
+                  Expanded(
+                    child: TextFormField(
+                      decoration:
+                          const InputDecoration(labelText: 'Url da Imagem'),
+                      textInputAction: TextInputAction.done,
+                      focusNode: _imageUrlFocus,
+                      keyboardType: TextInputType.url,
+                      controller: _imageUrlController,
+                    ),
+                  ),
+                  Container(
+                    margin: const EdgeInsets.only(
+                      top: 10,
+                      left: 10,
+                    ),
+                    height: 100,
+                    width: 100,
+                    decoration: BoxDecoration(
+                      border: Border.all(
+                        color: Colors.grey,
+                        width: 1,
+                      ),
+                    ),
+                    alignment: Alignment.center,
+                    child: _imageUrlController.text.isEmpty
+                        ? const Text('Informe a Url')
+                        : FittedBox(
+                            fit: BoxFit.cover,
+                            child: Image.network(_imageUrlController.text),
+                          ),
+                  )
+                ],
               ),
             ],
           ),
