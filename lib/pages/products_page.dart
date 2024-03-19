9488ac867e36e75871997e2737fe0c8325062361
IsraelRAlencar <israelalencar@gecontrolsistemas.com.br>
2024-03-18 20:16:23 -0400

    new - dando estilo ao productItem

--- lib/pages/products_page.dart
+++ lib/pages/products_page.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 import 'package:shop/components/app_drawer.dart';
+import 'package:shop/components/product_item.dart';
 
 import '../models/producto_list.dart';
 
@@ -19,7 +20,12 @@ class ProductsPage extends StatelessWidget {
         body: Padding(
           padding: const EdgeInsets.all(8),
           child: ListView.builder(
-            itemBuilder: (ctx, i) => Text(products.items[i].name),
+            itemBuilder: (ctx, i) => Column(
+              children: [
+                ProductItem(products.items[i]),
+                const Divider(),
+              ],
+            ),
             itemCount: products.itemsCount,
           ),
         ));
