af00597ca0c18ce142a0f3e1da10b137f6377b2a
IsraelRAlencar <israelalencar@gecontrolsistemas.com.br>
2024-03-22 12:24:25 -0400

    fix - tornando orders_page stateless e usando FutureBuilder

--- lib/pages/orders_page.dart
+++ lib/pages/orders_page.dart
@@ -7,19 +7,37 @@ import 'package:shop/models/order_list.dart';
 class OrdersPage extends StatelessWidget {
   const OrdersPage({super.key});
 
+  Future<void> _refreshProducts(BuildContext context) {
+    return Provider.of<OrderList>(context, listen: false).loadOrders();
+  }
+
   @override
   Widget build(BuildContext context) {
-    final OrderList orders = Provider.of<OrderList>(context);
-
     return Scaffold(
       appBar: AppBar(
         title: const Text("Pedidos"),
       ),
-      body: ListView.builder(
-        itemCount: orders.itemsCount,
-        itemBuilder: (ctx, i) => OrderWidget(
-          order: orders.items[i],
-        ),
+      body: FutureBuilder(
+        future: Provider.of<OrderList>(context, listen: false).loadOrders(),
+        builder: ((ctx, snapshot) {
+          if (snapshot.connectionState == ConnectionState.waiting) {
+            return const Center(child: CircularProgressIndicator());
+          } else if (snapshot.error != null) {
+            return const Center(child: Text('Ocorreu um erro!'));
+          } else {
+            return RefreshIndicator(
+              child: Consumer<OrderList>(
+                builder: (ctx, orders, child) => ListView.builder(
+                  itemCount: orders.itemsCount,
+                  itemBuilder: (ctx, i) => OrderWidget(
+                    order: orders.items[i],
+                  ),
+                ),
+              ),
+              onRefresh: () => _refreshProducts(context),
+            );
+          }
+        }),
       ),
       drawer: const AppDrawer(),
     );
--- lib/pages/products_page.dart
+++ lib/pages/products_page.dart
@@ -18,34 +18,35 @@ class ProductsPage extends StatelessWidget {
     final ProductList products = Provider.of(context);
 
     return Scaffold(
-        appBar: AppBar(
-          title: const Text('Gerenciar Produtos'),
-          actions: [
-            IconButton(
-              onPressed: () {
-                Navigator.of(context).pushNamed(
-                  AppRoutes.PRODUCT_FORM,
-                );
-              },
-              icon: const Icon(Icons.add),
-            ),
-          ],
-        ),
-        drawer: const AppDrawer(),
-        body: RefreshIndicator(
-          onRefresh: () => _refreshProducts(context),
-          child: Padding(
-            padding: const EdgeInsets.all(8),
-            child: ListView.builder(
-              itemBuilder: (ctx, i) => Column(
-                children: [
-                  ProductItem(products.items[i]),
-                  const Divider(),
-                ],
-              ),
-              itemCount: products.itemsCount,
-            ),
+      appBar: AppBar(
+        title: const Text('Gerenciar Produtos'),
+        actions: [
+          IconButton(
+            onPressed: () {
+              Navigator.of(context).pushNamed(
+                AppRoutes.PRODUCT_FORM,
+              );
+            },
+            icon: const Icon(Icons.add),
           ),
-        ));
+        ],
+      ),
+      drawer: const AppDrawer(),
+      body: RefreshIndicator(
+        onRefresh: () => _refreshProducts(context),
+        child: Padding(
+          padding: const EdgeInsets.all(8),
+          child: ListView.builder(
+            itemBuilder: (ctx, i) => Column(
+              children: [
+                ProductItem(products.items[i]),
+                const Divider(),
+              ],
+            ),
+            itemCount: products.itemsCount,
+          ),
+        ),
+      ),
+    );
   }
 }
